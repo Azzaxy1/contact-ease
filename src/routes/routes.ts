91ad17ed6ada230a4b57b09bd8af8fd6aa23f892
Azzaxy <abdurrohmanazis@gmail.com>
2024-09-25 21:59:05 +0700

    <feat> add page and form create contact

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import { body, validationResult, check } from "express-validator";
+
 import Contact from "../model/contact";
 
 const router = express.Router();
@@ -34,6 +36,48 @@ router.get("/contact", async (req, res) => {
     msg: req.flash("msg"),
   });
 });
+
+// Rute halaman form tambah data
+router.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    title: "Form Tambah Data Contact",
+    layout: "layouts/main-layout",
+    activeRoute: "contact",
+  });
+});
+
+// proses tambah data contact
+router.post(
+  "/contact",
+  body("nama").custom(async (value: string) => {
+    const duplicate = await Contact.findOne({ nama: value });
+    if (duplicate) {
+      throw new Error("Nama contact sudah terdaftar!");
+    }
+
+    return true;
+  }),
+  check("email", "Email tidak valid!").isEmail(),
+  check("nohp").isMobilePhone("id-ID").withMessage("No HandPhone tidak valid!"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("add-contact", {
+        title: "Form Tambah Data Contact",
+        layout: "layouts/main-layout",
+        activeRoute: "contact",
+        errors: errors.array(),
+        success: "Data berhasil dimasukan",
+      });
+    } else {
+      await Contact.insertMany(req.body);
+
+      // kirimkan flash message
+      req.flash("msg", "Data contact berhasil ditambahkan!");
+      res.redirect("/contact");
+    }
+  }
+);
 
 // Rute halaman detail contact
 router.get("/contact/:nama", async (req, res) => {
