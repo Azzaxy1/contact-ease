622ff24c2a0fdc7ffab019afafcb315df342efce
Azzaxy <abdurrohmanazis@gmail.com>
2024-09-26 21:47:45 +0700

    <refactor>setup env

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,5 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/mydb");
 
-// const contact1 = new Contact({
-//   nama: "Azis",
-//   nohp: "08583245345",
-//   email: "azis@gmail.com",
-// });
-
-// contact1.save().then((result) => console.log(result));
+const mongoURI: string =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mydb";
+mongoose.connect(mongoURI);
